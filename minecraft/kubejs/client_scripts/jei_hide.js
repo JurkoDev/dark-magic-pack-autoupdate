48c05819ebc130cbc2e15646b9a0253a405402fe
Darklord2996 <duggan.mick2006@gmail.com>
2025-10-17 23:29:01 +1000

    add information to custom recipe

--- minecraft/kubejs/client_scripts/jei_hide.js
+++ minecraft/kubejs/client_scripts/jei_hide.js
@@ -2,3 +2,16 @@ JEIEvents.hideItems((event) => {
     event.hide("/computercraft:*/");
     event.hide("/advancedperipherals:*/");
 });
+JEIEvents.information(event => {
+  // Creative Fluid Tank
+  event.addItem('create:creative_fluid_tank', [
+    '§6Note: This recipe requires a fully filled Mekanism Hohlraum! Even through the preview may not show it',
+    '§7Make sure it contains Fusion Fuel.'
+  ]);
+
+  // Creative Energy Cube
+  event.addItem('mekanism:creative_energy_cube', [
+    '§6Note: This recipe requires a fully filled Mekanism Hohlraum! Even through the preview may not show it',
+    '§7Make sure it contains Fusion Fuel.'
+  ]);
+});
