5efb1a5de238b9ed59fd59eb9d0dcda92c9544b3
Darklord2996 <duggan.mick2006@gmail.com>
2025-10-17 23:31:50 +1000

    See commit 48c0581

--- minecraft/kubejs/client_scripts/jei_hide.js
+++ minecraft/kubejs/client_scripts/jei_hide.js
@@ -2,3 +2,13 @@ JEIEvents.hideItems((event) => {
     event.hide("/computercraft:*/");
     event.hide("/advancedperipherals:*/");
 });
+JEIEvents.information(event => {
+    event.addItem('create:creative_fluid_tank', [
+        '§6Note: This recipe requires a fully filled Mekanism Hohlraum! Even through the preview may not show it',
+        '§7Make sure it contains Fusion Fuel.'
+    ]);
+    event.addItem('mekanism:creative_energy_cube', [
+        '§6Note: This recipe requires a fully filled Mekanism Hohlraum! Even through the preview may not show it',
+        '§7Make sure it contains Fusion Fuel.'
+    ]);
+});
